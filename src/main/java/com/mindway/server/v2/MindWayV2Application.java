7f95a57242c8b5ccca7010705cbe23205827c1f7
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-26 23:34:12 +0900

    setting

---
+++ src/main/java/com/mindway/server/v2/MindWayV2Application.java
@@ -0,0 +1,13 @@
+package com.mindway.server.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindWayV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindWayV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindway/server/v2/MindWayV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindway.server.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindWayV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
