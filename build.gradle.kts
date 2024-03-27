68b15e81f33d7f004a310858a0c5bef375d24366
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-27 19:51:32 +0900

    add :: 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,18 +19,31 @@ configurations {
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	// security
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
+	testImplementation("org.springframework.security:spring-security-test")
+
+	// database
 	runtimeOnly("com.mysql:mysql-connector-j")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	// GAuth
+	implementation ("com.github.GSM-MSG:GAuth-SDK-Java:v2.0.1")
+
+	// jwt
+	implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation ("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation ("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.withType<Test> {
