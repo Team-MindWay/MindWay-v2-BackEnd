60d05b03ba4562577cc6c23230e5839833f432d5
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-27 19:53:57 +0900

    add :: Member 추가

---
+++ src/main/java/com/mindway/server/v2/domain/member/entity/Member.java
@@ -0,0 +1,32 @@
+package com.mindway.server.v2.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue(generator = "UUID4")
+    @Column(name = "member_id")
+    private UUID id;
+
+    private String email;
+
+    private String name;
+
+    private String studentNum;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+}
