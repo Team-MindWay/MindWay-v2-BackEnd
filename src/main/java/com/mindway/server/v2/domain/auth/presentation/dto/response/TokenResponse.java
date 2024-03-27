02df434a74ca85158615376b1893867e58787846
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-28 01:45:19 +0900

    add :: TokenResponse 추가

---
+++ src/main/java/com/mindway/server/v2/domain/auth/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,21 @@
+package com.mindway.server.v2.domain.auth.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenResponse {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private LocalDateTime accessTokenExpiresIn;
+    private LocalDateTime refreshTokenExpiresIn;
+}
