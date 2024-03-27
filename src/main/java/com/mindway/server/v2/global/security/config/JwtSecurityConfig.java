2d96ef596ba6de77fb4b0c6649b7b5f739ecfa81
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-27 19:53:51 +0900

    add :: JwtSecurityConfig 추가

---
+++ src/main/java/com/mindway/server/v2/global/security/config/JwtSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.mindway.server.v2.global.security.config;
+
+import com.mindway.server.v2.global.security.filter.JwtFilter;
+import com.mindway.server.v2.global.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(jwtProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
