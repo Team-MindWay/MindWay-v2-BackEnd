7dcab1ff63466a7362dcf5a43ebafbc301a06498
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-28 01:45:03 +0900

    add :: StudentNum 임베드 추가

---
+++ src/main/java/com/mindway/server/v2/domain/member/entity/StudentNum.java
@@ -0,0 +1,27 @@
+package com.mindway.server.v2.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@MappedSuperclass
+@NoArgsConstructor
+public class StudentNum {
+    @Column(nullable = false, length = 1)
+    private Integer grade;
+
+    @Column(nullable = false, length = 1)
+    private Integer classNum;
+
+    @Column(nullable = false, length = 2)
+    private Integer number;
+
+    public StudentNum(Integer grade, Integer classNum, Integer number) {
+        this.grade = grade;
+        this.classNum = classNum;
+        this.number = number;
+    }
+
+}
