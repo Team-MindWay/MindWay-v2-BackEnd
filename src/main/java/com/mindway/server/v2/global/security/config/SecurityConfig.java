dd835757deafccdcf096a85d377870cc2cb5a0b4
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-28 01:44:01 +0900

    add :: signIn 메서드 설정 추가

--- src/main/java/com/mindway/server/v2/global/security/config/SecurityConfig.java
+++ src/main/java/com/mindway/server/v2/global/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.mindway.server.v2.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -43,6 +44,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
                                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/v2/auth/signin").permitAll()
                                 .anyRequest().authenticated()
                 )
 
