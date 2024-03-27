92aa4f7a0a0eae7cdbf68be1cafb6190ca20d028
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-27 19:52:24 +0900

    add :: GlobalException 추가

---
+++ src/main/java/com/mindway/server/v2/global/exception/GlobalException.java
@@ -0,0 +1,10 @@
+package com.mindway.server.v2.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GlobalException extends RuntimeException{
+    private  final ErrorCode errorCode;
+}
