7517680d2e76716f2bbdfc996cce5cb10827b3a7
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-28 01:41:51 +0900

    add :: jwt 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,9 +34,9 @@ dependencies {
 	implementation("com.github.GSM-MSG:GAuth-SDK-Java:v2.0.1")
 
 	// jwt
-	implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
-	implementation ("io.jsonwebtoken:jjwt-impl:0.11.5")
-	implementation ("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 	//redis
 	implementation ("org.springframework.boot:spring-boot-starter-data-redis:2.3.1.RELEASE")
