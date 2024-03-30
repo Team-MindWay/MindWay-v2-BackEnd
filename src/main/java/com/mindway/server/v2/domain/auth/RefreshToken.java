7d16efdd59b2bb0b5ebfa3b1a644d7400d3833a3
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-30 19:45:50 +0900

    add :: Indexed 어노테이션 추가

--- src/main/java/com/mindway/server/v2/domain/auth/RefreshToken.java
+++ src/main/java/com/mindway/server/v2/domain/auth/RefreshToken.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,6 +16,7 @@ public class RefreshToken {
 
     @Id
     private String refreshToken;
+    @Indexed
     private UUID memberId;
     private LocalDateTime expiredAt;
 
