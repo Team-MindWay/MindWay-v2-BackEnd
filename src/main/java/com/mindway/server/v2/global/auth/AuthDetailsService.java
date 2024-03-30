6eabf438e013d6fc32d46af624fa2202938a8480
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-30 19:41:07 +0900

    update :: String -> UUID

--- src/main/java/com/mindway/server/v2/global/auth/AuthDetailsService.java
+++ src/main/java/com/mindway/server/v2/global/auth/AuthDetailsService.java
@@ -4,19 +4,23 @@ import com.mindway.server.v2.domain.member.repository.MemberRepository;
 import com.mindway.server.v2.global.exception.ErrorCode;
 import com.mindway.server.v2.global.exception.MindWayException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return memberRepository.findByEmail(email)
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        return memberRepository.findById(UUID.fromString(id))
                 .map(AuthDetails::new)
                 .orElseThrow(() -> new MindWayException(ErrorCode.MEMBER_NOT_FOUND));
     }
