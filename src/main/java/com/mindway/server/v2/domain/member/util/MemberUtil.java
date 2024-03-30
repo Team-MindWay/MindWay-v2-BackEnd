0e9527ed2510946d1db35df69179a9715dfb89e0
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-30 19:45:12 +0900

    add :: getCurrentMember 로직 추가

--- src/main/java/com/mindway/server/v2/domain/member/util/MemberUtil.java
+++ src/main/java/com/mindway/server/v2/domain/member/util/MemberUtil.java
@@ -1,7 +1,21 @@
 package com.mindway.server.v2.domain.member.util;
 
+import com.mindway.server.v2.domain.auth.exception.MemberNotFoundException;
 import com.mindway.server.v2.domain.member.entity.Member;
+import com.mindway.server.v2.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
-public interface MemberUtil {
-    Member getCurrentMember();
+@Component
+@RequiredArgsConstructor
+public class MemberUtil {
+
+    private final MemberRepository memberRepository;
+
+    public Member getCurrentMember() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return memberRepository.findByEmail(email)
+                .orElseThrow(MemberNotFoundException::new);
+    }
 }
