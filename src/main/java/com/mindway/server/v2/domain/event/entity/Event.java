416f849be85a722a5adabfe62a936b14c8da7e6b
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-02 00:00:43 +0900

    delete :: Event 컬럼 name 설정 삭제

--- src/main/java/com/mindway/server/v2/domain/event/entity/Event.java
+++ src/main/java/com/mindway/server/v2/domain/event/entity/Event.java
@@ -17,22 +17,16 @@ public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "event_id")
     private Long id;
 
-    @Column(name = "event_title")
     private String title;
 
-    @Column(name = "event_content")
     private String content;
 
-    @Column(name = "event_img_url")
     private String img_url;
 
-    @Column(name = "event_started_at")
     private LocalDate started_at;
 
-    @Column(name = "event_endedat")
     private LocalDate ended_at;
 
 }
