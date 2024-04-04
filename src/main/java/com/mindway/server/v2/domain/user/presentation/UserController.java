6936b90bb75cbc1ded8e636de98409a1dd55d8c4
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-05 00:26:30 +0900

    add :: UserController 추가

--- src/main/java/com/mindway/server/v2/domain/user/presentation/UserController.java
+++ src/main/java/com/mindway/server/v2/domain/user/presentation/UserController.java
@@ -1,6 +1,8 @@
 package com.mindway.server.v2.domain.user.presentation;
 
 import com.mindway.server.v2.domain.user.presentation.dto.response.UserInfoResponse;
+import com.mindway.server.v2.domain.user.presentation.dto.response.MyOrdersResponse;
+import com.mindway.server.v2.domain.user.service.GetMyOrdersService;
 import com.mindway.server.v2.domain.user.service.UserInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/my")
 public class UserController {
 
     private final UserInfoService userInfoService;
+    private final GetMyOrdersService getMyOrdersService;
 
     @GetMapping
     public ResponseEntity<UserInfoResponse> getUserInfo() {
@@ -21,4 +26,9 @@ public class UserController {
         return ResponseEntity.ok(userInfoResponse);
     }
 
+    @GetMapping("/book")
+    public ResponseEntity<List<MyOrdersResponse>> getMyOrders() {
+        List<MyOrdersResponse> responses = getMyOrdersService.execute();
+        return ResponseEntity.ok(responses);
+    }
 }
