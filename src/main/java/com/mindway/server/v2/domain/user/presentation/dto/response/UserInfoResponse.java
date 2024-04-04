d44709f82b5b3d6316edd488862172c1a9c5a6b6
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-04 19:53:29 +0900

    update :: NoArgs -> AllArgs

--- src/main/java/com/mindway/server/v2/domain/user/presentation/dto/response/UserInfoResponse.java
+++ src/main/java/com/mindway/server/v2/domain/user/presentation/dto/response/UserInfoResponse.java
@@ -1,17 +1,12 @@
 package com.mindway.server.v2.domain.user.presentation.dto.response;
 
 import com.mindway.server.v2.domain.user.entity.Authority;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfoResponse {
     private String name;
     private Authority authority;
-
-    public UserInfoResponse(String name, Authority authority) {
-        this.name = name;
-        this.authority = authority;
-    }
 }
