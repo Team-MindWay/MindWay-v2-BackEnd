462180b11ac1ebdaaf2b4499f43706f6419095bf
taeyeon <tyeon42051@naver.com>
2024-04-06 11:06:11 +0900

    add :: endpoint 설정

--- src/main/java/com/mindway/server/v2/domain/order/presentation/OrdersController.java
+++ src/main/java/com/mindway/server/v2/domain/order/presentation/OrdersController.java
@@ -3,6 +3,7 @@ package com.mindway.server.v2.domain.order.presentation;
 import com.mindway.server.v2.domain.order.entity.BookType;
 import com.mindway.server.v2.domain.order.presentation.dto.request.OrderRequest;
 import com.mindway.server.v2.domain.order.service.BookRequestService;
+import com.mindway.server.v2.domain.order.service.DeleteBookOrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,12 +15,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v2/order")
 public class OrdersController {
     private final BookRequestService bookRequestService;
+    private final DeleteBookOrderService deleteBookOrderService;
 
     @PostMapping()
     public ResponseEntity<Void> bookRequest
             (@RequestParam BookType type, @RequestBody @Valid OrderRequest bookRequest) {
         bookRequestService.execute(type, bookRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{order_id}")
+    public ResponseEntity<Void> deleteBook (@PathVariable Long order_id) {
+        deleteBookOrderService.execute(order_id);
+        return ResponseEntity.noContent().build();
     }
 
 }
