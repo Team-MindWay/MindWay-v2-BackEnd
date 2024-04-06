5a74271ac16982f3bf1721a7cd0f656bbe644e97
taeyeon <tyeon42051@naver.com>
2024-04-07 00:57:46 +0900

    refactor :: dto 유효성 검사 추가

--- src/main/java/com/mindway/server/v2/domain/order/presentation/dto/request/OrderRequest.java
+++ src/main/java/com/mindway/server/v2/domain/order/presentation/dto/request/OrderRequest.java
@@ -1,12 +1,18 @@
 package com.mindway.server.v2.domain.order.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class OrderRequest {
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String author;
+
+    @NotBlank
     private String book_url;
 }
--- src/main/java/com/mindway/server/v2/domain/order/presentation/dto/request/OrderUpdateRequest.java
+++ src/main/java/com/mindway/server/v2/domain/order/presentation/dto/request/OrderUpdateRequest.java
@@ -1,10 +1,16 @@
 package com.mindway.server.v2.domain.order.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class OrderUpdateRequest {
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String author;
+
+    @NotBlank
     private String book_url;
 }
