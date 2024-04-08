77bcdc4d99f9d057833080e006d4df47971d25c6
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-08 15:20:59 +0900

    update :: MemberNotFoundException -> UserNotFoundException 수정

--- src/main/java/com/mindway/server/v2/domain/auth/exception/UserNotFoundException.java
+++ src/main/java/com/mindway/server/v2/domain/auth/exception/UserNotFoundException.java
@@ -3,9 +3,9 @@ package com.mindway.server.v2.domain.auth.exception;
 import com.mindway.server.v2.global.exception.ErrorCode;
 import com.mindway.server.v2.global.exception.MindWayException;
 
-public class MemberNotFoundException extends MindWayException {
+public class UserNotFoundException extends MindWayException {
 
-    public MemberNotFoundException() {
+    public UserNotFoundException() {
         super(ErrorCode.MEMBER_NOT_FOUND);
     }
 }
