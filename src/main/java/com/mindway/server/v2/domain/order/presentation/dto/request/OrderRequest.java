43ee5972555ae7edb99d2fa67623fb0bb7b8d094
taeyeon <tyeon42051@naver.com>
2024-04-11 20:06:43 +0900

    refactor :: url 정규식 제한

--- src/main/java/com/mindway/server/v2/domain/order/presentation/dto/request/OrderRequest.java
+++ src/main/java/com/mindway/server/v2/domain/order/presentation/dto/request/OrderRequest.java
@@ -1,6 +1,7 @@
 package com.mindway.server.v2.domain.order.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,5 +15,6 @@ public class OrderRequest {
     private String author;
 
     @NotBlank
+    @Pattern(regexp = "^https://www\\.yes24\\.com$", message = "YES24 링크를 입력해주세요")
     private String book_url;
 }
