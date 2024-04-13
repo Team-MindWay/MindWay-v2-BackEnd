04f9198ea9577c3455aa3d0e4b8c9b2df61c5e10
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-13 23:28:38 +0900

    add :: findByUser 추가

--- src/main/java/com/mindway/server/v2/domain/goal/repository/GoalRepository.java
+++ src/main/java/com/mindway/server/v2/domain/goal/repository/GoalRepository.java
@@ -4,6 +4,9 @@ import com.mindway.server.v2.domain.goal.entity.Goal;
 import com.mindway.server.v2.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GoalRepository extends JpaRepository<Goal, Long> {
     Boolean existsByUser(User user);
+    Optional<Goal> findByUser(User user);
 }
