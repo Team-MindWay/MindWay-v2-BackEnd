bb9903c67f9e4a47ec9a63b681cf0a2a8839c5cf
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-13 23:28:05 +0900

    add :: GoalGetService 추가

---
+++ src/main/java/com/mindway/server/v2/domain/goal/service/GoalGetService.java
@@ -0,0 +1,7 @@
+package com.mindway.server.v2.domain.goal.service;
+
+import com.mindway.server.v2.domain.goal.presentation.dto.response.GoalGetResponse;
+
+public interface GoalGetService {
+    GoalGetResponse execute();
+}
