583a700735a3cce39242c62723bc7597ad76b145
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-13 23:27:44 +0900

    add :: toDto 추가

--- src/main/java/com/mindway/server/v2/domain/goal/util/GoalConverter.java
+++ src/main/java/com/mindway/server/v2/domain/goal/util/GoalConverter.java
@@ -1,10 +1,13 @@
 package com.mindway.server.v2.domain.goal.util;
 
 import com.mindway.server.v2.domain.goal.entity.Goal;
+import com.mindway.server.v2.domain.goal.entity.Week;
+import com.mindway.server.v2.domain.goal.presentation.dto.response.GoalGetResponse;
 import com.mindway.server.v2.domain.user.entity.User;
 
 import java.time.LocalDate;
 
 public interface GoalConverter {
     Goal toEntity(User user, String created_at, String ended_at, Integer goal_count);
+    GoalGetResponse toDto(Week week, Integer goal_value, Integer now_count);
 }
