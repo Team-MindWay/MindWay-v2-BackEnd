61278c04c4af4a42ac4735f3cb278356c79f76d6
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-14 00:25:21 +0900

    add :: TimeZone 설정

--- src/main/java/com/mindway/server/v2/MindWayV2Application.java
+++ src/main/java/com/mindway/server/v2/MindWayV2Application.java
@@ -1,10 +1,19 @@
 package com.mindway.server.v2;
 
+import com.sun.jna.platform.mac.SystemB;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
+@EnableScheduling
 @SpringBootApplication
 public class MindWayV2Application {
+
+	@PostConstruct
+	public void started() {TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MindWayV2Application.class, args);
