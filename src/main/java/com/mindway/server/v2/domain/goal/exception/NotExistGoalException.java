3f79dfe8f1b40c66c3d64d4bfe0e67f50cd45cef
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-13 23:27:15 +0900

    add :: NotExistGoalException 추가

---
+++ src/main/java/com/mindway/server/v2/domain/goal/exception/NotExistGoalException.java
@@ -0,0 +1,10 @@
+package com.mindway.server.v2.domain.goal.exception;
+
+import com.mindway.server.v2.global.exception.ErrorCode;
+import com.mindway.server.v2.global.exception.MindWayException;
+
+public class NotExistGoalException extends MindWayException {
+    public NotExistGoalException() {
+        super(ErrorCode.EXIST_ALREADY_GOAL);
+    }
+}
