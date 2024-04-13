23498d399f9e693bf0527c152643170122f36634
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-13 15:43:49 +0900

    add :: GoalConverter 추가

---
+++ src/main/java/com/mindway/server/v2/domain/goal/util/impl/GoalConverterImpl.java
@@ -0,0 +1,22 @@
+package com.mindway.server.v2.domain.goal.util.impl;
+
+import com.mindway.server.v2.domain.goal.entity.Goal;
+import com.mindway.server.v2.domain.goal.util.GoalConverter;
+import com.mindway.server.v2.domain.user.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class GoalConverterImpl implements GoalConverter {
+
+    public Goal toEntity(User user, String created_at, String ended_at, Long goal_count) {
+        return Goal.builder()
+                .started_at(LocalDate.parse(created_at))
+                .ended_at(LocalDate.parse(ended_at))
+                .goal_count(goal_count)
+                .user(user)
+                .build();
+    }
+}
+
