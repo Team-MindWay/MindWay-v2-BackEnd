0464d2c5cbb78c8dd2ee3659090657231c60e789
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-17 10:03:18 +0900

    add :: 어드민 검증 로직 추가

--- src/main/java/com/mindway/server/v2/domain/notice/service/impl/NoticeAddServiceImpl.java
+++ src/main/java/com/mindway/server/v2/domain/notice/service/impl/NoticeAddServiceImpl.java
@@ -2,9 +2,11 @@ package com.mindway.server.v2.domain.notice.service.impl;
 
 import com.mindway.server.v2.domain.notice.converter.NoticeConverter;
 import com.mindway.server.v2.domain.notice.entity.Notice;
+import com.mindway.server.v2.domain.notice.exception.NotAccessStudentException;
 import com.mindway.server.v2.domain.notice.presentation.dto.request.NoticeAddRequestDto;
 import com.mindway.server.v2.domain.notice.repository.NoticeRepository;
 import com.mindway.server.v2.domain.notice.service.NoticeAddService;
+import com.mindway.server.v2.domain.user.entity.Authority;
 import com.mindway.server.v2.domain.user.entity.User;
 import com.mindway.server.v2.domain.user.util.UserUtil;
 import com.mindway.server.v2.global.annotation.ServiceWithTransaction;
@@ -20,6 +22,9 @@ public class NoticeAddServiceImpl implements NoticeAddService {
 
     public void execute(NoticeAddRequestDto noticeAddRequestDto) {
         User user = userUtil.getCurrentUser();
+
+        if (user.getAuthority() == Authority.ROLE_STUDENT)
+            throw new NotAccessStudentException();
 
         Notice notice = noticeConverter.toEntity(noticeAddRequestDto, user);
 
