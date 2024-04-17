46c7d6b0ae59cca86c63eeb0ad50f6d9bd9b430f
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-17 14:38:06 +0900

    add :: toDto 추가

--- src/main/java/com/mindway/server/v2/domain/notice/converter/impl/NoticeConverterImpl.java
+++ src/main/java/com/mindway/server/v2/domain/notice/converter/impl/NoticeConverterImpl.java
@@ -3,6 +3,7 @@ package com.mindway.server.v2.domain.notice.converter.impl;
 import com.mindway.server.v2.domain.notice.converter.NoticeConverter;
 import com.mindway.server.v2.domain.notice.entity.Notice;
 import com.mindway.server.v2.domain.notice.presentation.dto.request.NoticeAddRequestDto;
+import com.mindway.server.v2.domain.notice.presentation.dto.response.NoticeGetResponseDto;
 import com.mindway.server.v2.domain.user.entity.User;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,13 @@ public class NoticeConverterImpl implements NoticeConverter {
                 .title(noticeAddRequestDto.getTitle())
                 .content(noticeAddRequestDto.getContent())
                 .user(user)
+                .build();
+    }
+
+    public NoticeGetResponseDto toDto(Notice notice) {
+        return NoticeGetResponseDto.builder()
+                .title(notice.getTitle())
+                .content(notice.getContent())
                 .build();
     }
 }
