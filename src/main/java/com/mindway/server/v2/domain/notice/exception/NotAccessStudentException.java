e3193c3ac212cd4f8af5d590483396e214fc03b3
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-17 10:03:26 +0900

    add :: NotAccessStudentException 추가

---
+++ src/main/java/com/mindway/server/v2/domain/notice/exception/NotAccessStudentException.java
@@ -0,0 +1,10 @@
+package com.mindway.server.v2.domain.notice.exception;
+
+import com.mindway.server.v2.global.exception.ErrorCode;
+import com.mindway.server.v2.global.exception.MindWayException;
+
+public class NotAccessStudentException extends MindWayException {
+    public NotAccessStudentException() {
+        super(ErrorCode.NOT_ACCESS_STUDENT);
+    }
+}
