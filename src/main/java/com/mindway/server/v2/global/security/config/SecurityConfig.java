91fc836ad205ce40698b6c7c6e04987780d6dfe2
taeyeon <tyeon42051@naver.com>
2024-04-18 15:49:52 +0900

    add :: endpoint 설정

--- src/main/java/com/mindway/server/v2/global/security/config/SecurityConfig.java
+++ src/main/java/com/mindway/server/v2/global/security/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
                                 // book
                                 .requestMatchers(HttpMethod.POST, "/api/v2/book").authenticated()
                                 .requestMatchers(HttpMethod.DELETE, "api/v2/book/{book_id}").authenticated()
+                                .requestMatchers(HttpMethod.PATCH, "api/v2/book/{book_id}").authenticated()
 
                                 // notice
                                 .requestMatchers(HttpMethod.POST, "/api/v2/notice").hasAnyAuthority(Authority.ROLE_TEACHER.name(), Authority.ROLE_HELPER.name())
