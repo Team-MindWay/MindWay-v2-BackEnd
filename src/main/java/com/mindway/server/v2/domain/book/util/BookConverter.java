5a086167bece3cc150b14db5b20472668654cc1e
taeyeon <tyeon42051@naver.com>
2024-04-19 10:45:40 +0900

    add :: book converter toDto 추상화 및 구현

--- src/main/java/com/mindway/server/v2/domain/book/util/BookConverter.java
+++ src/main/java/com/mindway/server/v2/domain/book/util/BookConverter.java
@@ -2,8 +2,11 @@ package com.mindway.server.v2.domain.book.util;
 
 import com.mindway.server.v2.domain.book.entity.Book;
 import com.mindway.server.v2.domain.book.presentation.dto.request.BookWriteRequest;
+import com.mindway.server.v2.domain.book.presentation.dto.response.BookInfoResponse;
 import com.mindway.server.v2.domain.user.entity.User;
 
 public interface BookConverter {
     Book toEntity (BookWriteRequest bookWriteRequest, User user);
+
+    BookInfoResponse toDto (Book book);
 }
