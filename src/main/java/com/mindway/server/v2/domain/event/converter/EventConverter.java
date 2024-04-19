7f4cd2623cf0a230d58f9fc115dfd5fc5518f9db
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-19 11:59:49 +0900

    update :: toEntity에 Status 추가

--- src/main/java/com/mindway/server/v2/domain/event/converter/EventConverter.java
+++ src/main/java/com/mindway/server/v2/domain/event/converter/EventConverter.java
@@ -1,9 +1,10 @@
 package com.mindway.server.v2.domain.event.converter;
 
 import com.mindway.server.v2.domain.event.entity.Event;
+import com.mindway.server.v2.domain.event.entity.Status;
 import com.mindway.server.v2.domain.event.presentation.dto.request.EventWriteRequestDto;
 import com.mindway.server.v2.domain.user.entity.User;
 
 public interface EventConverter {
-    Event toEntity(EventWriteRequestDto eventWriteRequestDto, User user, String image_url);
+    Event toEntity(EventWriteRequestDto eventWriteRequestDto, User user, String image_url, Status status);
 }
