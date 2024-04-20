fc6d5b3f6e0b2f698429f07f38966f1a6e823786
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-21 00:47:24 +0900

    add :: findByStatus 추가

--- src/main/java/com/mindway/server/v2/domain/event/repository/EventRepository.java
+++ src/main/java/com/mindway/server/v2/domain/event/repository/EventRepository.java
@@ -1,7 +1,11 @@
 package com.mindway.server.v2.domain.event.repository;
 
 import com.mindway.server.v2.domain.event.entity.Event;
+import com.mindway.server.v2.domain.event.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByStatus(Status status);
 }
