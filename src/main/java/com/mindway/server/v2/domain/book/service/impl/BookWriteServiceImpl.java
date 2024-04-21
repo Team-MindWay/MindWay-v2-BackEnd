ecd270d0a38863acf69371283c72723e6f306334
taeyeon <tyeon42051@naver.com>
2024-04-22 00:14:31 +0900

    update :: BookWriteServiceImpl

--- src/main/java/com/mindway/server/v2/domain/book/service/impl/BookWriteServiceImpl.java
+++ src/main/java/com/mindway/server/v2/domain/book/service/impl/BookWriteServiceImpl.java
@@ -5,6 +5,8 @@ import com.mindway.server.v2.domain.book.presentation.dto.request.BookWriteReque
 import com.mindway.server.v2.domain.book.repository.BookRepository;
 import com.mindway.server.v2.domain.book.service.BookWriteService;
 import com.mindway.server.v2.domain.book.util.BookConverter;
+import com.mindway.server.v2.domain.rank.entity.Ranks;
+import com.mindway.server.v2.domain.rank.repository.RankRepository;
 import com.mindway.server.v2.domain.user.entity.User;
 import com.mindway.server.v2.domain.user.util.UserUtil;
 import com.mindway.server.v2.global.annotation.ServiceWithTransaction;
@@ -17,11 +19,22 @@ public class BookWriteServiceImpl implements BookWriteService {
     private final BookRepository bookRepository;
     private final BookConverter bookConverter;
     private final UserUtil userUtil;
+    private final RankRepository rankRepository;
 
     public void execute(BookWriteRequest bookWriteRequest) {
         User user = userUtil.getCurrentUser();
 
         Book book = bookConverter.toEntity(bookWriteRequest, user);
+        accrue(user);
         bookRepository.save(book);
+    }
+
+    private void accrue(User user) {
+        Ranks rank = rankRepository.findByUser(user);
+        if (rank == null) {
+            rank = new Ranks(user);
+        }
+        rank.accrue();
+        rankRepository.save(rank);
     }
 }
