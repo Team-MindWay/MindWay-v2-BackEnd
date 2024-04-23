e5cfb8942660b71cfdc378539f36d2b6ff160e98
taeyeon <tyeon42051@naver.com>
2024-04-23 22:33:16 +0900

    add :: RecRepository

--- src/main/java/com/mindway/server/v2/domain/rec/repository/RecRepository.java
+++ src/main/java/com/mindway/server/v2/domain/rec/repository/RecRepository.java
@@ -1,7 +1,11 @@
 package com.mindway.server.v2.domain.rec.repository;
 
 import com.mindway.server.v2.domain.rec.entity.Rec;
+import com.mindway.server.v2.domain.rec.entity.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecRepository extends JpaRepository<Rec, Long> {
+    List<Rec> findByType (Type type);
 }
