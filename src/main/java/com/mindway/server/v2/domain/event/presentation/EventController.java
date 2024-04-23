180fa1e15614512513cd8815ca778278f20ac978
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-23 17:56:18 +0900

    add :: getsEvent 추가

--- src/main/java/com/mindway/server/v2/domain/event/presentation/EventController.java
+++ src/main/java/com/mindway/server/v2/domain/event/presentation/EventController.java
@@ -1,6 +1,9 @@
 package com.mindway.server.v2.domain.event.presentation;
 
+import com.mindway.server.v2.domain.event.entity.Status;
 import com.mindway.server.v2.domain.event.presentation.dto.request.EventWriteRequestDto;
+import com.mindway.server.v2.domain.event.presentation.dto.response.EventGetsResponseDto;
+import com.mindway.server.v2.domain.event.service.EventGetsService;
 import com.mindway.server.v2.domain.event.service.EventWriteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +12,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/event")
 public class EventController {
 
     private final EventWriteService eventWriteService;
+    private final EventGetsService eventGetsService;
 
     @PostMapping
     public ResponseEntity<Void> writeEvent(
@@ -23,6 +29,12 @@ public class EventController {
     ) {
         eventWriteService.execute(eventWriteRequestDto, image);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventGetsResponseDto>> getsEvent(@RequestParam(required = false) Status status) {
+        List<EventGetsResponseDto> responses = eventGetsService.execute(status);
+        return ResponseEntity.ok(responses);
     }
 
 
