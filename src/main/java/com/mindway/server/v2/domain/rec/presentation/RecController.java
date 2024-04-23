255f09ceded8224300248373dfe1fd6cb44887d9
taeyeon <tyeon42051@naver.com>
2024-04-23 22:32:46 +0900

    add :: endpoint 설정

--- src/main/java/com/mindway/server/v2/domain/rec/presentation/RecController.java
+++ src/main/java/com/mindway/server/v2/domain/rec/presentation/RecController.java
@@ -2,6 +2,8 @@ package com.mindway.server.v2.domain.rec.presentation;
 
 import com.mindway.server.v2.domain.rec.entity.Type;
 import com.mindway.server.v2.domain.rec.presentation.dto.reqest.WriteRecRequest;
+import com.mindway.server.v2.domain.rec.presentation.dto.response.RecInfoResponse;
+import com.mindway.server.v2.domain.rec.service.GetRecService;
 import com.mindway.server.v2.domain.rec.service.WriteRecService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/recommend")
 public class RecController {
     private final WriteRecService writeRecService;
+    private final GetRecService getRecService;
 
     @PostMapping
     public ResponseEntity<Void> writeRecBook (@RequestBody @Valid WriteRecRequest writeRecRequest, @RequestParam Type type){
         writeRecService.execute(writeRecRequest, type);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RecInfoResponse>> getRecBooks (@RequestParam Type type) {
+        List<RecInfoResponse> responses = getRecService.execute(type);
+        return ResponseEntity.ok(responses);
     }
 }
