684176d6bb98c0bfe0d7bc40c4c0ad1326215e3d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-05-02 18:27:56 +0900

    add :: blacklist에 등록하는 로직 추가

--- src/main/java/com/mindway/server/v2/domain/auth/service/impl/LogoutServiceImpl.java
+++ src/main/java/com/mindway/server/v2/domain/auth/service/impl/LogoutServiceImpl.java
@@ -7,6 +7,8 @@ import com.mindway.server.v2.domain.auth.service.LogoutService;
 import com.mindway.server.v2.domain.user.entity.User;
 import com.mindway.server.v2.domain.user.util.UserUtil;
 import com.mindway.server.v2.global.annotation.ServiceWithTransaction;
+import com.mindway.server.v2.global.redis.RedisUtil;
+import com.mindway.server.v2.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -15,14 +17,17 @@ public class LogoutServiceImpl implements LogoutService {
 
     private final RefreshRepository refreshRepository;
     private final UserUtil userUtil;
+    private final RedisUtil redisUtil;
+    private final JwtProvider jwtProvider;
 
-    public void execute() {
+    public void execute(String accessToken) {
         User user = userUtil.getCurrentUser();
 
         RefreshToken validRefreshToken = refreshRepository.findByMemberId(user.getId())
                 .orElseThrow(ExpiredRefreshTokenException::new);
 
         refreshRepository.deleteById(validRefreshToken.getRefreshToken());
+        redisUtil.setBlackList(accessToken, "access_Token", jwtProvider.getExpiration(accessToken));
     }
 
 }
