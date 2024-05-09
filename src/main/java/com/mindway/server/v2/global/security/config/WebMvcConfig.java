9e460a8ed26345e9ae657b43ff16101dd1898598
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-05-09 18:30:43 +0900

    add :: WebMvcConfig 추가

---
+++ src/main/java/com/mindway/server/v2/global/security/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.mindway.server.v2.global.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
