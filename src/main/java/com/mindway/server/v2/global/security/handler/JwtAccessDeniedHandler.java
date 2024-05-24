653f7ae2db6c7753737d2b25d7ac0a893a58d57d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-05-24 14:01:21 +0900

    add :: log 추가

--- src/main/java/com/mindway/server/v2/global/security/handler/JwtAccessDeniedHandler.java
+++ src/main/java/com/mindway/server/v2/global/security/handler/JwtAccessDeniedHandler.java
@@ -3,6 +3,7 @@ package com.mindway.server.v2.global.security.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        log.info("=====ACCESS DENIED=====");
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
