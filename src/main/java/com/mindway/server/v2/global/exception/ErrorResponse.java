b6f463b026c57628df50befccd4fe50589851825
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-05-24 14:00:33 +0900

    delete :: 불필요한 import 삭제

--- src/main/java/com/mindway/server/v2/global/exception/ErrorResponse.java
+++ src/main/java/com/mindway/server/v2/global/exception/ErrorResponse.java
@@ -1,8 +1,6 @@
 package com.mindway.server.v2.global.exception;
 
 import lombok.Builder;
-import lombok.Getter;
 
 @Builder
-public record ErrorResponse(int status, String message) {
-}
+public record ErrorResponse(int status, String message) {}
