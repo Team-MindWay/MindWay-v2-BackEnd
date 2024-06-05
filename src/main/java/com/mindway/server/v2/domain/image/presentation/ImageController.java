50de3706a085466dc7ee64a90d870ebe56790a38
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-05 20:21:27 +0900

    add :: RequestMapping 설정 추가

--- src/main/java/com/mindway/server/v2/domain/image/presentation/ImageController.java
+++ src/main/java/com/mindway/server/v2/domain/image/presentation/ImageController.java
@@ -5,12 +5,14 @@ import com.mindway.server.v2.domain.image.service.ImageUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v2/image")
 public class ImageController {
 
     private final ImageUploadService imageUploadService;
