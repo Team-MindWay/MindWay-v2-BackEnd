c3dd03b266392796eeee80f610f6851159bb1a92
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-11 15:12:28 +0900

    add :: Goal 스케줄러 Logging 추가

--- src/main/java/com/mindway/server/v2/domain/goal/service/impl/GoalsDeleteServiceImpl.java
+++ src/main/java/com/mindway/server/v2/domain/goal/service/impl/GoalsDeleteServiceImpl.java
@@ -4,15 +4,19 @@ import com.mindway.server.v2.domain.goal.repository.GoalRepository;
 import com.mindway.server.v2.domain.goal.service.GoalsDeleteService;
 import com.mindway.server.v2.global.annotation.ServiceWithTransaction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @ServiceWithTransaction
 @RequiredArgsConstructor
+@Slf4j
 public class GoalsDeleteServiceImpl implements GoalsDeleteService {
 
     private final GoalRepository goalRepository;
 
     public void execute() {
+        log.info("===================목표 초기화 스케줄러 시작===================");
         goalRepository.deleteAll();
+        log.info("===================목표 초기화 스케줄러 끝===================");
     }
 
 }
