33a1842d9896b33622b5d2e08e0cc6110bdc7079
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-07-10 11:50:44 +0900

    add :: url 추가

--- src/main/java/com/mindway/server/v2/global/security/config/WebMvcConfig.java
+++ src/main/java/com/mindway/server/v2/global/security/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://mindway.vercel.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
