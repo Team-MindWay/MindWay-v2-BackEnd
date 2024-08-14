a7e63de16928ae349114e14b5dcb0de9f85f8cc9
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-08-14 15:00:10 +0900

    update :: 리프레쉬 로직 수정

--- src/main/java/com/mindway/server/v2/domain/auth/service/impl/ReissueTokenServiceImpl.java
+++ src/main/java/com/mindway/server/v2/domain/auth/service/impl/ReissueTokenServiceImpl.java
@@ -12,6 +12,9 @@ import com.mindway.server.v2.global.annotation.ServiceWithTransaction;
 import com.mindway.server.v2.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @ServiceWithTransaction
 public class ReissueTokenServiceImpl implements ReissueTokenService {
@@ -31,7 +34,11 @@ public class ReissueTokenServiceImpl implements ReissueTokenService {
 
         refreshRepository.deleteById(refreshEntity.getRefreshToken());
 
-        return jwtProvider.generateTokenDto(user.getId());
+        TokenResponse tokenResponse = jwtProvider.generateTokenDto(user.getId());
+
+        saveRefreshToken(tokenResponse.getRefreshToken(), user.getId(), tokenResponse.getRefreshTokenExpiresIn());
+
+        return tokenResponse;
     }
 
     private void saveRefreshToken(String refreshToken, UUID memberId, LocalDateTime expiredAt) {
